f8a3f7e9bbbc7903e73f8511d991d2b9ad545719
Luis Angel Aboytes Rocha <luis.aboytes@hopers.com>
2019-11-19 12:17:31 -0600

    Se integro el diseño del login y de pageNotFound responsivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent} from './app-components/login/login.component'
+import { PageNotFoundComponent} from  './app-components/page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"**", component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./app-components/login/login.component";
+import { PageNotFoundComponent } from "./app-components/page-not-found/page-not-found.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, PageNotFoundComponent],
+  imports: [BrowserModule, TooltipModule.forRoot(), AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
